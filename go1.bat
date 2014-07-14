7f1bea1ff6810f399d183339996c062f8aaeeeba
Flaxsearch <git@flax.co.uk>
2014-07-14 16:00:38 +0100

    now using central paths file

--- go1.bat
+++ go1.bat
@@ -1,8 +1,10 @@
 rem go1.bat
+rem This is one of the three batch files to be run to set up Clade on Windows. 
+rem Each must run in its own command line shell
 
-call \work\py27.bat
-call \work\java6.bat
+call paths.bat
+
 python classify.py import data\socpsy.csv
-copy solr-conf\*.* \work\tools\apache-solr-3.6.0\example\solr\conf /Y
-cd \work\tools\apache-solr-3.6.0\example
+copy solr-conf\*.* %SOLRPATH%\example\solr\collection1\conf /Y
+cd %SOLRPATH%\example
 java -jar start.jar &

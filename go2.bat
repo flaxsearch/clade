3199482c7af410c7f106a70b3f0b2de0af0b970f
Flaxsearch <git@flax.co.uk>
2014-07-14 16:01:01 +0100

    now using central paths file

--- go2.bat
+++ go2.bat
@@ -1,6 +1,8 @@
 rem go2.bat
+rem This is one of the three batch files to be run to set up Clade on Windows. 
+rem Each must run in its own command line shell
 
-call \work\py27.bat
-call \work\java6.bat
+call paths.bat
+
 cd stanford-ner-2011-09-14
 server.bat

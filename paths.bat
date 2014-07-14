1066d386e8f313a741bfb6d3caf142a65727ab6d
Flaxsearch <git@flax.co.uk>
2014-07-14 16:00:00 +0100

    Create paths.bat

---
+++ paths.bat
@@ -0,0 +1,8 @@
+rem paths.bat sets up Java and Python paths - adjust to your system
+
+rem path to Java
+path=C:\Program Files (x86)\Java\jre7\bin;%path%
+rem path to Python
+path=%path%;c:\work\tools\python27;path=%path%;c:\work\tools\python27\scripts;
+rem path to Apache Solr
+set solrpath=\work\tools\solr-4.7.2

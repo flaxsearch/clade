4cf6669e3279476d451db00ff0bdd80adb2a3fb3
epugh <epugh@opensourceconnections.com>
2019-09-10 17:18:42 -0400

    force commits

--- delete-all.sh
+++ delete-all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-URL=http://localhost:8983/solr/update
+URL=http://localhost:8983/solr/update?commit=true
 curl $URL --data-binary '<delete><query>*:*</query></delete>' -H 'Content-type:application/xml'
 echo

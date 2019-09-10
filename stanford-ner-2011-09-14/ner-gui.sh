404458f396c09bdf76aa06ce62dbb5a79036ac85
epugh <epugh@opensourceconnections.com>
2019-09-10 17:04:06 -0400

    updates for python3

--- stanford-ner-2011-09-14/ner-gui.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -mx500m -cp "stanford-ner.jar:" edu.stanford.nlp.ie.crf.NERGUI
--- stanford-ner-2011-09-14/ner.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -mx700m -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier classifiers/all.3class.distsim.crf.ser.gz -textFile $1
